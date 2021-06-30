844861861e04bde5d82e12db995433b682a26653
Muhammad Hassan <57257840+Muhammad-Hassan1000@users.noreply.github.com>
2021-06-30 13:49:19 +0500

    added solution

---
+++ Math/FindAngleMBC.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from math import *
+AB = float(input())
+BC = float(input())
+print(str(int(round(degrees(atan2(AB,BC))))) + "°")
